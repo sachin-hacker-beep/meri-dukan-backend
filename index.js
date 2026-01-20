02d4f4ae12d1cb2442374901f39c9240727a03c1
sachin sahu <sachinsahu19004@gmail.com>
2026-01-20 20:14:09 +0530

    fixing cors issue

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5175',
+    origin: 'http://localhost:5173',
     credentials: true,
 }));
 app.use(cookieParser());
