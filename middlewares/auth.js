947a11d67f22c87c014e18e4ad969166981924b5
sachin sahu <sachinsahu19004@gmail.com>
2026-01-21 16:13:21 +0530

    fixing signup 4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ dotenv.config();
 export const verifyToken=(req,res,next)=>{
     const token = req.cookies.token;
     if(!token){
-        return res.status(401).json({message: "Unauthorized: No token provided"});
+        return res.status(405).json({message: "Unauthorized: No token provided"});
     }
     try{
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
