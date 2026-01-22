624ac8482452337ac49869ef409e2607a3e6f913
sachin sahu <sachinsahu19004@gmail.com>
2026-01-22 21:16:18 +0530

    fixing signup 10

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ app.use(express.json());
 app.use(cors({
   origin: "https://meri-dukan-frontend-qujb.vercel.app",
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.options("*", cors());
