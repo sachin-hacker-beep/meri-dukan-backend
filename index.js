b41b986436943e925482905965441c3b3b7549e1
sachin sahu <sachinsahu19004@gmail.com>
2026-01-25 14:30:08 +0530

    changing token management 3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://meri-dukan-frontend-qujb.vercel.app",
+  origin: "http://localhost:5173",
 //   credentials: true,
 }));
 
