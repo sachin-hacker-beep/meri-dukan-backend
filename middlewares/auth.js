0feb8359d84f5bbc7911ce0006099f94e50c11da
sachin sahu <sachinsahu19004@gmail.com>
2026-01-25 19:32:05 +0530

    testing  middleware 4

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 export const verifyToken=(req,res,next)=>{
     // const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : null;
+    console.log("token is ", token);
     const authHead = req.headers.authorization || req.headers.Authorization;
     if(!authHead || !authHead.startsWith("Bearer ")){
         return res.status(401).json({message:"No token Provided"})
