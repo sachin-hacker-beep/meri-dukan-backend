9509787296000b7d9ba2e5158aaebb68ad7c6d2d
sachin sahu <sachinsahu19004@gmail.com>
2026-01-29 17:31:05 +0530

    implementing cart api 2

--- index.js
+++ index.js
@@ -152,12 +152,13 @@ app.delete('/cart/remove/:productID/:selectedSize', verifyToken, async(req,res)=
         if(!findItem){
             return res.status(404).json({message: "Product not found in cart"});
         }
-        if(findItem.quantity > 1){
-            findItem.quantity -= 1;
-            res.status(201).json({message: "Product quantity decreased in cart"});
-        }
-        cart.products= cart.products.filter(item => !(item.productID.toString() === productID && item.selectedSize === selectedSize ));
-        await cart.save();
+        console.log("Found Item to remove:", findItem);
+        // if(findItem.quantity > 1){
+        //     findItem.quantity -= 1;
+        //     res.status(201).json({message: "Product quantity decreased in cart"});
+        // }
+        // cart.products= cart.products.filter(item => !(item.productID.toString() === productID && item.selectedSize === selectedSize ));
+        // await cart.save();
         res.status(200).json({message: "Product Removed from cart"});
     }
     catch(err){
